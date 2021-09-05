01f9a4374dab8ebdb5409e7da4d0735550ca2df5
Roman Lapitski <romalap1@gmail.com>
2021-09-05 21:49:45 +0300

    fsda

--- check.sh
+++ check.sh
@@ -1,19 +1,9 @@
-#!/bin/bash 
-curl --write-out %{http_code} --silent --output /dev/null http://192.168.97.200:3000 > commandResult
-env.status = readFile('commandResult').trim()
-sh "echo ${env.status}"
-if (env.status == '200') {
-    currentBuild.result = "SUCCESS"
-}
-else {
-def folder=params.Folder
-def version=params.Version
-sh '''./rollback.sh "''' + host + '''" "''' + folder + '''" "''' + version +'''"'''
-sh '''curl --write-out %{http_code} --silent --output /dev/null http://"''' + host + '''":80 > commandResult'''
-env.status = readFile('commandResult').trim()
-sh "echo ${env.status}"
-if (env.status == '200') {
-    currentBuild.result = "SUCCESS"
- }
- else {
-currentBuild.result = "FAILURE"
+#!/bin/bash
+curl --write-out %{http_code} --silent --output /dev/null http://192.168.97.200:3000 > Result
+if grep 200 Result
+  then
+    exit
+else
+echo "VERSION=1.8.2" > .env
+sudo docker-compose up -d
+fi
